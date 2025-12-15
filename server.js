14bde77f381a511aa7b9f7a156de7cc052674a68
johnayuk <john1234ayuk@gmail.com>
2025-12-15 09:01:05 +0100

    fiex server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(helmet())
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "https://www.dtc.com.ng", "https://admin.dtc.com.ng"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://www.dtc.com.ng", "https://admin.dtc.com.ng", "https://dtc-admin-1.onrender.com"],
   credentials: true,
 }))
 app.use(morgan("dev"))
